beb6785634d0d7ce8dc52236db75663bb068b741
YeJinHong <teepij@naver.com>
2022-05-26 22:55:52 +0900

    API 9번 광고리스트 구현

--- src/main/java/com/example/demo/src/categories/CategoryProvider.java
+++ src/main/java/com/example/demo/src/categories/CategoryProvider.java
@@ -1,8 +1,7 @@
-package com.example.demo.src.event;
+package com.example.demo.src.categories;
 
 import com.example.demo.config.BaseException;
-import com.example.demo.src.event.model.GetEventTopRes;
-import com.example.demo.src.user.UserDao;
+import com.example.demo.src.categories.model.GetCategoryRes;
 import com.example.demo.utils.JwtService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,24 +13,24 @@ import java.util.List;
 import static com.example.demo.config.BaseResponseStatus.DATABASE_ERROR;
 
 @Service
-public class EventProvider {
-    private final EventDao eventDao;
+public class CategoryProvider {
+    private final CategoryDao categoryDao;
     private final JwtService jwtService;
-
 
     final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    public EventProvider(EventDao eventDao, JwtService jwtService) {
-        this.eventDao = eventDao;
+    public CategoryProvider(CategoryDao categoryDao, JwtService jwtService){
+        this.categoryDao = categoryDao;
         this.jwtService = jwtService;
     }
 
-    public List<GetEventTopRes> getEventTopList() throws BaseException{
-        try{
-            List<GetEventTopRes> getEventTopList = eventDao.getEventTopList();
-            return getEventTopList;
+    public List<GetCategoryRes> getCategoryList() throws BaseException {
+        try {
+            List<GetCategoryRes> getCategoryList = categoryDao.getCategoryList();
+            return getCategoryList;
         } catch (Exception exception){
+            System.out.println(exception);
             throw new BaseException(DATABASE_ERROR);
         }
     }
