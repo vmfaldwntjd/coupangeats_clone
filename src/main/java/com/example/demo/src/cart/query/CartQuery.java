695c69d32e631ac8966ac0292bfcade95907ebd9
YeJinHong <teepij@naver.com>
2022-05-30 13:42:17 +0900

    delete project for gitignore

--- src/main/java/com/example/demo/src/cart/query/CartQuery.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.src.cart.query;
-
-public class CartQuery {
-
-    public static String createCartQuery = "INSERT INTO cart(user_id, user_address_id, restaurant_id) VALUES(?, ?, ?);";
-
-
-}
