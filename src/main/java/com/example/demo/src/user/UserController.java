34986aadfa968cb63a9abb516eeefdb55d866353
appleJuice <home@appleJuices-MacBook-Pro.local>
2022-05-31 22:37:04 +0900

    gitignore 수정

--- src/main/java/com/example/demo/src/user/UserController.java
+++ src/main/java/com/example/demo/src/user/UserController.java
@@ -196,13 +196,13 @@ public class UserController {
     //  JSON은 HTTP 통신 시, 데이터를 주고받을 때 많이 쓰이는 데이터 포맷.
     @GetMapping("/{userId}/orders") // (GET) 127.0.0.1:9000/app/users
     // GET 방식의 요청을 매핑하기 위한 어노테이션
-    public BaseResponse<List<GetOrderRes>> getOrders(@PathVariable("userId") int userId, @RequestParam(required = false) int deliveryStatus) {
+    public BaseResponse<List<GetOrderRes>> getOrders(@PathVariable("userId") int userId, @RequestParam(required = false) int delivery_status) {
         //  @RequestParam은, 1개의 HTTP Request 파라미터를 받을 수 있는 어노테이션(?뒤의 값). default로 RequestParam은 반드시 값이 존재해야 하도록 설정되어 있지만, (전송 안되면 400 Error 유발)
         //  지금 예시와 같이 required 설정으로 필수 값에서 제외 시킬 수 있음
         //  defaultValue를 통해, 기본값(파라미터가 없는 경우, 해당 파라미터의 기본값 설정)을 지정할 수 있음
         try {
             //delivery_status의 존재 유무 확인하는 예외처리는 생략하였습니다.
-            List<GetOrderRes> getOrderRes = userProvider.getOrdersByDeliveryStatus(userId, deliveryStatus);
+            List<GetOrderRes> getOrderRes = userProvider.getOrdersByDeliveryStatus(userId, delivery_status);
             return new BaseResponse<>(getOrderRes);
         } catch (BaseException exception) {
             return new BaseResponse<>((exception.getStatus()));
@@ -297,4 +297,6 @@ public class UserController {
             return new BaseResponse<>((exception.getStatus()));
         }
     }
+
+
 }
